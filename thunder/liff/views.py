93af0b4d15e9d0d960e021024b6fb244a3cb942d
本田一期 <honda.ichigo@outlook.jp>
2021-03-12 21:04:59 +0900

    [ADD]：テンプレートファイルによるHello, World!

--- thunder/liff/views.py
+++ thunder/liff/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'liff/index.html')
