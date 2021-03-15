c7c6161e9a1dde27477d85a43a956bd4d6a5adb9
本田一期 <honda.ichigo@outlook.jp>
2021-03-15 12:38:51 +0900

    [UPDATE]：ToDoアプリの削除機能調整、CSSファイルの適用

--- thunder/todo/views.py
+++ thunder/todo/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from datetime import datetime
 from django.urls import reverse
+from django.views.decorators.http import require_POST
 
 from .models import Todo
 
@@ -10,7 +11,7 @@ app_name = 'todo'
 # Create your views here.
 def index(request):
     todos = Todo.objects.all()
-    return render(request, 'todo/index.html', {'todos': todos})
+    return render(request, 'index.html', {'todos': todos})
 
 def add(request):
     todo = Todo(
@@ -21,6 +22,7 @@ def add(request):
     todo.save()
     return HttpResponseRedirect(reverse('index', args=()))
 
+@require_POST
 def remove(request, todo_id):
     todo = get_object_or_404(Todo, pk = todo_id)
     todo.delete()
