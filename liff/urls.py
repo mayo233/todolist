0de4881a3e7379570c5a5175c05d0a17c68cb61c
本田一期 <honda.ichigo@outlook.jp>
2021-03-16 08:01:45 +0900

    [ADD]：LIFFのWebhook接続

--- liff/urls.py
+++ liff/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='hello'),
+    path('callback/', views.callback, name='callback'),
 ]
--- liff/views.py
+++ liff/views.py
@@ -1,6 +1,32 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponseBadRequest, HttpResponse
 from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt
+from .models import Post, EmailPush, LinePush
 
 # Create your views here.
 def index(request):
     return render(request, 'liff/index.html')
+
+@csrf_exempt
+def callback(request):
+    """ラインの友達追加時に呼び出され、ラインのIDを登録する。"""
+    if request.method == 'POST':
+        request_json = json.loads(request.body.decode('utf-8'))
+        events = request_json['events']
+        line_user_id = events[0]['source']['userId']
+
+        # チャネル設定のWeb hook接続確認時にはここ。このIDで見に来る。
+        if line_user_id == 'thunder_auth':
+            pass
+
+        # 友達追加時・ブロック解除時
+        elif events[0]['type'] == 'follow':
+            LinePush.objects.create(user_id=line_user_id)
+
+        # アカウントがブロックされたとき
+        elif events[0]['type'] == 'unfollow':
+            LinePush.objects.filter(user_id=line_user_id).delete()
+
+    return HttpResponse()
+    
