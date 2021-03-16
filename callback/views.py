b456bc78b3e126680e285847e3192a3b1121436f
本田一期 <honda.ichigo@outlook.jp>
2021-03-16 11:01:45 +0900

    line確認用

--- callback/views.py
+++ callback/views.py
@@ -18,12 +18,12 @@ def callback(request):
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
 
-        try:
-            events = parser.parse(body, signature)
-        except InvalidSignatureError:
-            return HttpResponseForbidden()
-        except LineBotApiError:
-            return HttpResponseBadRequest()
+        # try:
+        events = parser.parse(body, signature)
+        # except InvalidSignatureError:
+        #     return HttpResponseForbidden()
+        # except LineBotApiError:
+        #     return HttpResponseBadRequest()
 
         for event in events:
             if isinstance(event, MessageEvent):
